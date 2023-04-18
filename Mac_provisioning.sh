7b6b4343888c0a84ed43b6de20f33d865f0e7da8
uoNate <130566875+uoNate@users.noreply.github.com>
2023-04-18 14:46:26 -0400

    Update Mac_provisioning.sh
    adding confirmation for name change as well as custom choice
    adding a confirmation to delete two keychain items if Company portal fails to install the profile

--- Mac_provisioning.sh
+++ Mac_provisioning.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+Clear
 echo "***********************************************"
-echo "This script changes the computer name to either XXXX-SERIALNUM or your preference"
-echo "It changes the time server to time.uottawa.ca"
+echo "This script changes the computer name to your preference"
 echo "Deactivate Filevault (encryption if active)"
 echo "Removes Sophos if present"
 echo "Checks whether the uOttawa MDM profile is installed, if not, installs Company Portal using this script https://github.com/microsoft/shell-intune-samples/blob/master/macOS/Apps/Company%20Portal/installCompanyPortal.sh"
@@ -13,55 +12,101 @@ echo "     "
 echo "if this script fails to run, use: xattr -d com.apple.quarantine Mac_provisioning.sh  "
 echo "***********************************************"
 
+bold=$(tput bold)
+normal=$(tput sgr0)
 #Get the Mac serial number
 serial=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 current_name=$(scutil --get ComputerName)
 
 #Display the Mac Serial number
-echo "This Mac has the following serial number: $serial"
-echo "it is also named: $current_name"
-echo "***********************************************" 
+echo "${bold}Mac Serial Number: $serial${normal}"
+echo "Current name: ${bold}$current_name${normal}"
+echo "***********************************************"
+read -p "Do you want to rename your computer? (y (auto.)/n/custom): " response
 
-#Prompt user for computer name option
-echo "Do you want to use a basic 4 digit Prefix (e.g. 1803, 2101, 3700, etc.)" 
-read -p "Or customize the computer name to your liking? (BASIC/CUSTOM): " answer
+if [[ "$response" == "" || "$response" == "y" ]];
+then
+    # Prompt the user for a prefix (required)
+    read -p "Enter a prefix for the computer name (2101, 1803-CV, etc.): " prefix
+    # Prompt the user for a suffix (optional)
+    read -p "Enter a suffix for the computer name (-prf, -P, etc.), hit Return for no suffix: " suffix
+    
+    # If the user didn't enter anything for the suffix, set it to empty
+    if [[ "$suffix" == "" ]]; then
+        addsuffix="n"
+    else
+        addsuffix="y"
+    fi
 
-#If user chooses to use a 4-digit Banner code
-if [ "$answer" == "BASIC" ] || [ "$answer" == "basic" ]; then
+    # Trim the serial number if necessary to ensure the total character limit is 15
+    if [[ "$addsuffix" == "y" ]]; then
+        namelength=$(( ${#prefix} + ${#serial} + ${#suffix} + 2 ))
+        if [[ "$namelength" -gt 15 ]]; then
+            serial=$(echo "$serial" | cut -c 1-$(( 15 - ${#prefix} - ${#suffix} - 2 )))
+        fi
+    else
+        namelength=$(( ${#prefix} + ${#serial} + 1 ))
 
-#Prompt user for 4-digit number
-read -p "Enter a 4-digit number for the prefix: " XXXX
+        if [[ "$namelength" -gt 15 ]]; then
+            serial=$(echo "$serial" | cut -c 1-$(( 15 - ${#prefix} - 1 )))
+        fi
+    fi
 
-#Append serial number to computer name in this form BANN-SERIAL
-computer_name="$XXXX-$serial"
-if [ ${#computer_name} -gt 15 ]; then
-computer_name=${computer_name:0:15}
-fi
+    # Create the computer name using the prefix, serial number, and suffix (if applicable)
+    if [[ "$addsuffix" == "y" ]]; then
+        name="$prefix-$serial-$suffix"
+    else
+        name="$prefix-$serial"
+    fi
 
+    # Remove any double dashes in the name
+    name=$(echo "$name" | sed 's/--/-/g')
+
+    # Check for double dashes due to prefix or suffix
+    while [[ "$name" == *"--"* ]]; do
+        name=$(echo "$name" | sed 's/--/-/g')
+    done
+
+# Ask the user to confirm the name
+echo "The computer will be set to: $name" 
+read -p "Is this name correct? (y/n): " confirm
+
+# If the user confirms the name, set the computer name
+if [[ "$confirm" == "y" ]]; then
+    sudo scutil --set ComputerName "$name"
+    sudo scutil --set HostName "$name"
+    sudo scutil --set LocalHostName "$name"
+    dscacheutil -flushcache
+   echo "The computer name has been set to: $name"
 else
-#Prompt user for computer name
-read -p "Enter a custom name for this computer: " computer_name
+    echo "The computer name has not been changed."
 fi
 
-#Set the Mac Name to this result and display it
-echo "***********************************************"
-echo "Trying to set computer name to: $computer_name ..."
-sudo scutil --set ComputerName "$computer_name"
-sudo scutil --set HostName "$computer_name"
-sudo scutil --set LocalHostName "$computer_name"
-
-#check if name is correctly applied
-Echo "Computer name is now:"
-sudo scutil --get ComputerName
-
-
-# Set time server and update time
-echo "***********************************************"
-echo "Setting Time server to time.uottawa.ca"
-sudo systemsetup -setusingnetworktime on
-sudo systemsetup -setnetworktimeserver time.uottawa.ca &> /dev/null
-sudo sntp time.uottawa.ca
-
+elif [ "$response" = "custom" ]; then
+  read -p "Enter a custom name for this computer (max 15 characters): " custom_name
+  while [[ ${#custom_name} -gt 15 ]]; do
+    read -p "Name must be 15 characters or less. Please enter a custom name: " custom_name
+  done
+  echo "The computer name you've entered is: ${bold}$custom_name${normal}."
+  read -p "Is this correct? (y/n): " confirm_name
+  if [[ $confirm_name =~ ^[Yy]$ ]]; then
+    computer_name="$custom_name"
+    echo "The new computer name will be: ${bold}$computer_name${normal}."
+    read -p "Do you want to proceed with changing the computer name? (y/n): " confirm_change
+    if [[ $confirm_change =~ ^[Yy]$ ]]; then
+      sudo scutil --set ComputerName "$computer_name"
+      sudo scutil --set HostName "$computer_name"
+      sudo scutil --set LocalHostName "$computer_name"
+      echo "The computer name has been changed to: ${bold}$computer_name${normal}."
+    else
+      echo "The computer name will remain: ${bold}$current_name${normal}."
+    fi
+  else
+    echo "Please re-enter a custom name."
+  fi
+else
+  echo "moving on"
+fi
 
 # Remove Sophos
 echo "***********************************************"
@@ -106,14 +151,15 @@ echo "Do you want to check for uOttawa MDM Profile? (y/n)"
 read answer
 
 if [ "$answer" == "y" ]; then
+	clear
   if [ -f "/private/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound" ]; then
     if grep -q "ottawa" "/private/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound"; then
-echo "***********************************************"      
-echo "uOttawa MDM Profile found!"
-    echo "Add $computer_name to the Defender Group (might not be available instantly)"
+      echo "***********************************************"      
+      echo "uOttawa MDM Profile found!"
+      echo "Add $computer_name to the Defender Group (might not be available instantly)"
     else
-      echo "uOttawa MDM Profile not found."
-    echo "Do you want to install Company Portal app (which will require manual intervention)? (y/n)"
+      echo "uOttawa MDM Profile ${bold}not${normal} found."
+      echo "Do you want to install Company Portal app (which will require manual intervention)? (y/n)"
       read install_answer
       if [ "$install_answer" == "y" ]; then
         # Download and run the Company Portal installation script
@@ -122,7 +168,32 @@ echo "uOttawa MDM Profile found!"
         # Open the Company Portal app
         open "/Applications/Company Portal.app"
 
-# /Applications/Company Portal.app/Contents
+        echo "${bold}The Company Portal has opened to manually enrol this Mac. Were you successful in installing the profile?"
+        echo -e "(Answering 'No' will delete Microsoft identity cache in the keychain, resulting in a requirement to login again onto the Microsoft Account$){normal} (y/n)"
+        read install_success
+
+        if [ "$install_success" == "n" ]; then
+          security delete-internet-password -s msoCredentialSchemeADAL
+	  security delete-generic-password -l 'com.microsoft.adalcache'
+
+          echo -e "${bold}Please test the profile enrolment again. Did it work this time?${normal} (y/n)"
+          read test_success
+
+          if [ "$test_success" == "n" ]; then
+            echo -e "Open Keychain Access (/Applications/Utilities), and manually delete everything that has 'identities', 'Microsoft', and 'adal' in the name."
+            echo -e "Please test the profile enrolment again. Did it work this time? (y/n)"
+            read test_success_again
+
+            if [ "$test_success_again" == "n" ]; then
+              echo -e "Please contact NateTech for further assistance."
+            fi
+          fi
+        else
+          echo "***********************************************"
+          echo "Once the Profile has been installed through Company portal"
+          echo "Add $computer_name to the Defender Group (might not be available instantly)."
+          echo "***********************************************"
+        fi
       fi
     fi
   else
@@ -135,11 +206,22 @@ echo "uOttawa MDM Profile found!"
 
       # Open the Company Portal app
       open "/Applications/Company Portal.app"
+
+      echo -e "${bold}The Company Portal has opened to manually enrol this Mac. Were you successful in installing the profile?${normal} (y/n)"
+      read install_success
+
+      if [ "$install_success" == "n" ]; then
+        security delete-internet-password -s msoCredentialSchemeADAL # delete the internet password
+        echo -e "${bold}Please try to enroll the profile again. Did it work this time?${normal} (y/n)" # ask the user to test the profile enrollment again
+        read enroll_success
+
+  if [ "$enroll_success" == "n" ]; then
+    echo -e "${bold}Open Keychain access (/Applications/Utilities), and manually delete everything that has \"identities\", \"Microsoft\" and \"adal\" in the name. Did this solve the issue?${normal} (y/n)" # prompt the user to open Keychain Access and delete specific items
+    read keychain_success
+
+    if [ "$keychain_success" == "n" ]; then
+      echo -e "${bold}Please contact Nathan for further assistance.${normal}" # prompt the user to contact Nathan if the issue still persists
     fi
-    echo "***********************************************"
-    echo "Once the Profile has been installed through Company portal"
-    echo "Add $computer_name to the Defender Group (might not be available instantly)."
-    echo "***********************************************"
   fi
 fi
 
@@ -236,4 +318,7 @@ echo "Ending Script - Added to the domain, adding Admin groups"
     else echo "Ending Script - Added to the domain, without adding Admin groups"
 fi
 else echo "Ending Script - Not joined to Domain"
+fi
+fi
+fi
 fi
